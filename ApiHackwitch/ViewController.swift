4b0ac4dcfea8b598350cdd8f3c91d32b6c6f46b3
ZAYVIAN WILSON <rl107037@rlas-macair-23.rlas-116.org>
2018-10-01 12:05:50 -0500

    Initial Commit

---
+++ ApiHackwitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApiHackwitch
+//
+//  Created by ZAYVIAN WILSON on 10/1/18.
+//  Copyright © 2018 ZAYVIAN WILSON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
