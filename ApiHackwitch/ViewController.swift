39a059c36e056fc6794e50e517b132cb027ee96a
ZAYVIAN WILSON <rl107037@rlas-macair-23.rlas-116.org>
2018-10-03 12:38:50 -0500

    i did a lot of things

--- ApiHackwitch/ViewController.swift
+++ ApiHackwitch/ViewController.swift
@@ -9,11 +9,61 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
+    
+    var sources = [[String: String]]()
+    var apiKey = "679376b10aad42019ff49513ad555602"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-
-}
-
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    self.parse(json: json)
+                    return
+                }
+            }
+        }
+        self.loadError()
+    }
+    }
+    
+    func parse(json: JSON) {
+        for result in json["sources"].arrayValue {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            sources.append(source)
+            tableView.reloadData()
+            DispatchQueue.main.async {
+                [unowned self] in
+                self.tableView.reloadData()
+           }
+            
+        }
+    }
+    
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",message: "There was a problem loading the newsfeed",
+            preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK",style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
